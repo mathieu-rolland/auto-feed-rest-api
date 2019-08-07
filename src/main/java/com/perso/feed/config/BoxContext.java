fecfbedc84ee3162ea7e86785029b3e8e5101130
Mathieu Rolland <mathieu.rolland2705@gmail.com>
2019-08-07 23:10:27 +0200

    Ajout d'un emulateur pour la GPIO

--- src/main/java/com/perso/feed/config/BoxContext.java
+++ src/main/java/com/perso/feed/config/BoxContext.java
@@ -1,13 +1,12 @@
 package com.perso.feed.config;
 
+import com.perso.feed.material.emulator.IPinProvider;
 import com.perso.feed.model.Drawer;
 import com.perso.feed.model.DrawerStateEnum;
 import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalInput;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.PinState;
-import com.pi4j.io.gpio.RaspiPin;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
@@ -16,39 +15,28 @@ import lombok.extern.slf4j.Slf4j;
 
 @Getter
 @Slf4j
-public class BoxContextSingleton {
+public class BoxContext {
 
 	private Drawer drawer1;
 	private Drawer drawer2;
 	
 	private GpioPinDigitalOutput ledPin;
 	
-	private static BoxContextSingleton singleton;
-	
-	public static BoxContextSingleton getInstance() {
-		if( singleton == null ) {
-			singleton = new BoxContextSingleton();
-			singleton.initContext();
-		}
-		return singleton;
-	}
-	
-	public void initContext() {
+	public void setupGpio( GpioController gpio , IPinProvider pinProvider ) {
 		
-		final GpioController gpio = GpioFactory.getInstance();
 		
-		ledPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "MyLED", PinState.HIGH);
+		ledPin = gpio.provisionDigitalOutputPin( pinProvider.getPin00() , "MyLED", PinState.HIGH);
 		ledPin.setShutdownOptions(true, PinState.LOW);
 		
-		GpioPinDigitalOutput motor1Pin1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_25, "motor1Pin1", PinState.LOW);
+		GpioPinDigitalOutput motor1Pin1 = gpio.provisionDigitalOutputPin( pinProvider.getPin25() , "motor1Pin1", PinState.LOW);
 		motor1Pin1.setShutdownOptions(true, PinState.LOW);
 		
-		GpioPinDigitalOutput motor1Pin2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_24, "motor1Pin2", PinState.LOW);
+		GpioPinDigitalOutput motor1Pin2 = gpio.provisionDigitalOutputPin(pinProvider.getPin24(), "motor1Pin2", PinState.LOW);
 		motor1Pin2.setShutdownOptions(true, PinState.LOW);
 		
 		drawer1 = new Drawer( "motor1Pin1" , DrawerStateEnum.CLOSED, motor1Pin1, motor1Pin2);
 		
-		GpioPinDigitalInput course = gpio.provisionDigitalInputPin( RaspiPin.GPIO_01 , "course1"  );
+		GpioPinDigitalInput course = gpio.provisionDigitalInputPin( pinProvider.getPin01() , "course1"  );
 		
 		course.addListener( new GpioPinListenerDigital() {
 			@Override
